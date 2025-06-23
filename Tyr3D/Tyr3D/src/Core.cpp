b473a0aa5f6ea4de851fb87911118b8510ec5ae0
Tstebbs <2201393@uad.ac.uk>
2025-06-23 16:57:53 +0100

    Fixed: Glad

--- Tyr3D/Tyr3D/src/Core.cpp
+++ Tyr3D/Tyr3D/src/Core.cpp
@@ -1,15 +1,40 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "Core.h"
+#include <iostream>
 
 namespace Tyr3D
 {
-
-
 	void Core::CreateWindow(int width, int height)
 	{
 		glfwInit();
-		GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+		GLFWwindow* window = glfwCreateWindow(1240, 1000, "My Title", NULL, NULL);
+
+		if (window == NULL)
+		{
+			std::cout << "Window Failed";
+			glfwTerminate();
+			return;
+			
+		}
+		
+		glfwMakeContextCurrent(window);
+
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+		{
+			std::cerr << "ERROR: gladLoadGLLoader failed\n";
+			return;
+		}
+
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return;
 	}
 }
 
