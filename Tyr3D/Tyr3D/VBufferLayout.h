6508450cb3b1b6ada57a9f930c4a9d2eb6b69116
Tstebbs <2201393@uad.ac.uk>
2025-06-30 20:56:27 +0100

    Started Vbuffer implementation

--- Tyr3D/Tyr3D/VBufferLayout.h
+++ Tyr3D/Tyr3D/VBufferLayout.h
@@ -1,5 +1,29 @@
 #pragma once
-class VBufferLayout
-{
-};
 
+#include<iostream>
+#include<vector>
+
+#include<glad/glad.h>
+namespace Tyr3D
+{
+	struct VBufferElement
+	{
+		unsigned int type;
+		unsigned int count;
+		unsigned int stride;
+		bool isnormalised;
+	};
+
+
+	class VBufferLayout
+	{
+	private:
+		std::vector<VBufferElement> memElements;
+
+
+	public:
+		VBufferLayout();
+
+	};
+
+}
